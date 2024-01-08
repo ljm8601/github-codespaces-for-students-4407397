d66a1d091a6ae123e6d28f1307270c5f311e127e
ljm8601 <155979248+ljm8601@users.noreply.github.com>
2024-01-08 13:28:13 +0000

    Changed Learn react to link here

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Link here
           </a>
         </p>
       </header>
